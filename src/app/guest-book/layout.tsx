c52ee60aabcee0e2c69cd08dd80d45feea2e6f9d
bforbilly24 <halimputra0701@gmail.com>
2024-03-22 17:43:08 +0700

    feat(page): add /guest-book/layout

---
+++ src/app/guest-book/layout.tsx
@@ -0,0 +1,99 @@
+import { Suspense } from 'react'
+import { FaRegEnvelope } from 'react-icons/fa'
+import { BsWhatsapp, BsInstagram, BsLinkedin } from 'react-icons/bs'
+
+import { Input } from '@/components/atoms/input'
+import { AsideLink } from '@/components/atoms/aside-link'
+import { FadeInStagger, FadeIn } from '@/components/atoms/fade-in'
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/atoms/accordion'
+
+import { auth } from '@/lib/auth'
+import { ENV } from '@/lib/constants'
+import { createPost } from '@/lib/actions'
+import { generateSEO } from '@/lib/generateSEO'
+import { SignInBtn, SendBtn } from './action-buttons'
+
+const title = 'guest-book'
+const description =
+  'Leave a lasting imprint on my digital canvas! Sign in and share your thoughts, greetings, or anecdotes on my guest-book page. Your messages contribute to the heart and soul of my online community. Connect with us through your words and be a part of the vibrant conversations happening on my website. Your messages matter, so take a moment to make your mark and be heard!'
+const url = `${ENV.NEXT_PUBLIC_WEBSITE_URL}/guest-book`
+const image = `${ENV.NEXT_PUBLIC_WEBSITE_URL}/api/og?title=${title}`
+
+export const metadata = generateSEO(title, description, image, url)
+
+export default async function AboutLayout({ children }: { children: React.ReactNode }) {
+  const session = await auth()
+  return (
+    <section className='grid grid-cols-12 overflow-hidden'>
+      <aside className='md:col-span-3 lg:col-span-2 border-r border-lines md:block hidden overflow-y-auto'>
+        <Accordion type='single' collapsible defaultValue='item-0'>
+          {data.map((item, i) => (
+            <AccordionItem value={`item-${i}`} key={i}>
+              <AccordionTrigger className='border-b border-lines px-5 py-2.5 text-left' data-umami-event='accordion-guest-book'>
+                {item.title}
+              </AccordionTrigger>
+              <AccordionContent className='mt-5 space-y-1'>
+                <FadeInStagger faster>
+                  {item.list.map((listItem, j) => (
+                    <FadeIn key={j}>
+                      <Suspense fallback={<>Loading...</>}>
+                        <AsideLink href={listItem.href} startWith='/about'>
+                          <span className='shrink-0'>{listItem.icon}</span>
+                          {listItem.title}
+                        </AsideLink>
+                      </Suspense>
+                    </FadeIn>
+                  ))}
+                </FadeInStagger>
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </aside>
+      <section className='md:col-span-9 lg:col-span-10 col-span-12 overflow-y-auto relative h-[84dvh] md:h-auto'>
+        <FadeIn className='p-5 space-y-2'>
+          <form action={createPost} className='col-span-full flex items-center justify-between gap-x-2.5'>
+            <div className='flex-1 relative'>
+              <Input name='desc' id='desc' placeholder='This is really cool!' aria-labelledby='desc' />
+            </div>
+            {!session ? <SignInBtn /> : <SendBtn />}
+          </form>
+          {children}
+        </FadeIn>
+      </section>
+    </section>
+  )
+}
+
+const data = [
+  {
+    title: 'Contacts',
+    list: [
+      {
+        title: 'Email',
+        href: 'mailto:halimputra0701@gmail.com',
+        icon: <FaRegEnvelope className='w-4 h-4' />
+      },
+    //   {
+    //     title: 'Upwork',
+    //     href: 'https://www.upwork.com/freelancers/',
+    //     icon: <TbBrandUpwork className='w-4 h-4' />
+    //   },
+      {
+        title: 'WhatsApp',
+        href: 'https://wa.me/+6285156644103',
+        icon: <BsWhatsapp className='w-4 h-4' />
+      },
+      {
+        title: 'LinkedIn',
+        href: 'https://www.linkedin.com/in/halimp/',
+        icon: <BsLinkedin className='w-4 h-4' />
+      },
+      {
+        title: 'Instagram',
+        href: 'https://www.instagram.com/bforbilly24/',
+        icon: <BsInstagram className='w-4 h-4' />
+      }
+    ]
+  }
+]
