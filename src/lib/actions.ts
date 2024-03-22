f2a467f31f50f27c4b1d2102ae1a1c0c2e3a7395
bforbilly24 <halimputra0701@gmail.com>
2024-03-22 18:07:10 +0700

    refac(pages): comment prisma db

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,33 +1,33 @@
 'use server'
 import { revalidatePath } from 'next/cache'
 
-import { db } from './prisma'
-import { auth } from './auth'
+// import { db } from './prisma'
+// import { auth } from './auth'
 import * as Wakatime from '@/types/wakatimeResponse'
 
-export const createPost = async (formData: FormData) => {
-  const session = await auth()
-  const desc = formData.get('desc') as string
-  if (!session || !desc) return
-  await db.post.create({
-    data: {
-      desc: desc,
-      userId: session.user.id
-    }
-  })
+// export const createPost = async (formData: FormData) => {
+//   const session = await auth()
+//   const desc = formData.get('desc') as string
+//   if (!session || !desc) return
+//   await db.post.create({
+//     data: {
+//       desc: desc,
+//       userId: session.user.id
+//     }
+//   })
 
-  revalidatePath('/guest-book')
-}
+//   revalidatePath('/guest-book')
+// }
 
-export const deletePost = async (id: number) => {
-  await db.post.delete({
-    where: {
-      id: id
-    }
-  })
+// export const deletePost = async (id: number) => {
+//   await db.post.delete({
+//     where: {
+//       id: id
+//     }
+//   })
 
-  revalidatePath('/guest-book')
-}
+//   revalidatePath('/guest-book')
+// }
 
 export const weeklyCodingActivity = async () => {
   const res = await fetch('https://wakatime.com/share/@bforbilly/a6fada39-4105-48f9-acef-032cd0643f73.json', {
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,41 +1,41 @@
-import NextAuth from 'next-auth'
-import GitHub from 'next-auth/providers/github'
-import { PrismaAdapter } from '@auth/prisma-adapter'
-import { DefaultSession } from 'next-auth'
+// import NextAuth from 'next-auth'
+// import GitHub from 'next-auth/providers/github'
+// import { PrismaAdapter } from '@auth/prisma-adapter'
+// import { DefaultSession } from 'next-auth'
 
-import { ENV } from './constants'
-import { db } from './prisma'
+// import { ENV } from './constants'
+// import { db } from './prisma'
 
-declare module 'next-auth' {
-  interface Session extends DefaultSession {
-    user: {
-      id: string
-    } & DefaultSession['user']
-  }
-}
+// declare module 'next-auth' {
+//   interface Session extends DefaultSession {
+//     user: {
+//       id: string
+//     } & DefaultSession['user']
+//   }
+// }
 
-export const {
-  handlers: { GET, POST },
-  auth
-} = NextAuth({
-  secret: ENV.AUTH_SECRET,
-  adapter: PrismaAdapter(db),
-  providers: [
-    GitHub({
-      clientId: ENV.GITHUB_CLIENT_ID,
-      clientSecret: ENV.GITHUB_CLIENT_SECRET
-    })
-  ],
-  callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id
-      }
-    })
-  },
-  pages: {
-    error: '/'
-  }
-})
+// export const {
+//   handlers: { GET, POST },
+//   auth
+// } = NextAuth({
+//   secret: ENV.AUTH_SECRET,
+//   adapter: PrismaAdapter(db),
+//   providers: [
+//     GitHub({
+//       clientId: ENV.GITHUB_CLIENT_ID,
+//       clientSecret: ENV.GITHUB_CLIENT_SECRET
+//     })
+//   ],
+//   callbacks: {
+//     session: ({ session, user }) => ({
+//       ...session,
+//       user: {
+//         ...session.user,
+//         id: user.id
+//       }
+//     })
+//   },
+//   pages: {
+//     error: '/'
+//   }
+// })
