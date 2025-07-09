ae8cfb2cb74b4dcf357b2c9d80cddeeb4dca2a84
Halim <halimputra0701@gmail.com>
2025-07-09 15:49:45 +0700

    feat(lib): adding lib prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,10 @@
-// import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
+import { ENV } from '@/types/environment'
 
-// import { ENV } from './constants'
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
 
-// const globalForPrisma = globalThis as unknown as {
-//   prisma: PrismaClient | undefined
-// }
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
-// export const db =
-//   globalForPrisma.prisma ??
-//   new PrismaClient({
-//     log: ENV.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error']
-//   })
-
-// if (ENV.NODE_ENV !== 'production') globalForPrisma.prisma = db
+if (ENV.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
