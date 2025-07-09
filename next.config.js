5139f64bc5d76b41f7377ad44ab351606ca7b156
Halim <halimputra0701@gmail.com>
2025-07-09 15:13:01 +0700

    refactor(config): adding remote patterns

--- next.config.js
+++ next.config.js
@@ -1,30 +1,52 @@
-const { withContentlayer } = require('next-contentlayer')
+const { withContentlayer } = require('next-contentlayer');
 
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  experimental: {
-    webpackBuildWorker: true,
-    serverActions: {
-      enabled: true
-    }
-  },
-  logging: {
-    fetches: {
-      fullUrl: true
-    }
-  },
-  redirects() {
-    return [
-      {
-        source: '/about',
-        destination: '/about/personal.ts',
-        permanent: true
-      }
-    ]
-  }
-}
+	pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'avatars.githubusercontent.com',
+				port: '',
+				pathname: '/u/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'wakatime.com',
+				port: '',
+				pathname: '/photo/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'res.cloudinary.com',
+				port: '',
+				pathname: '/bforbilly/**',
+			},
+		],
+	},
+	experimental: {
+		webpackBuildWorker: true,
+		serverActions: {
+			enabled: true,
+		},
+	},
+	logging: {
+		fetches: {
+			fullUrl: true,
+		},
+	},
+	redirects() {
+		return [
+			{
+				source: '/about',
+				destination: '/about/personal.ts',
+				permanent: true,
+			},
+		];
+	},
+};
 
-module.exports = withContentlayer(nextConfig)
+module.exports = withContentlayer(nextConfig);
