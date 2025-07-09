77819b67d98c8868f1d969654207572915dc9c0c
Halim <halimputra0701@gmail.com>
2025-07-09 15:41:47 +0700

    feat(component): adding component atoms responsive indicator

--- src/components/atoms/responsive-indicator.tsx
+++ src/components/atoms/responsive-indicator.tsx
@@ -1,14 +1,20 @@
+'use client'
+import { ClientOnly } from './client-only'
+import { ENV } from '@/types/environment'
+
 export const ResponsiveIndicator = () => {
-  if (process.env.NODE_ENV === 'production') return null
+  if (ENV.NODE_ENV === 'production') return null
 
   return (
-    <div className='fixed bottom-1 left-1 z-[999999999] flex h-6 w-6 items-center justify-center rounded-full bg-black p-3 font-mono text-xs text-white'>
-      <div className='block sm:hidden'>xs</div>
-      <div className='hidden sm:block md:hidden'>sm</div>
-      <div className='hidden md:block lg:hidden'>md</div>
-      <div className='hidden lg:block xl:hidden'>lg</div>
-      <div className='hidden xl:block 2xl:hidden'>xl</div>
-      <div className='hidden 2xl:block'>2xl</div>
-    </div>
+    <ClientOnly>
+      <div className='fixed bottom-1 left-1 z-[999999999] flex h-6 w-6 items-center justify-center rounded-full bg-black p-3 font-mono text-xs text-white'>
+        <div className='block sm:hidden'>xs</div>
+        <div className='hidden sm:block md:hidden'>sm</div>
+        <div className='hidden md:block lg:hidden'>md</div>
+        <div className='hidden lg:block xl:hidden'>lg</div>
+        <div className='hidden xl:block 2xl:hidden'>xl</div>
+        <div className='hidden 2xl:block'>2xl</div>
+      </div>
+    </ClientOnly>
   )
 }
