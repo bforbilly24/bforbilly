c5a4f01fe8889da1c38d86dd8063713ab2c10e77
Halim <halimputra0701@gmail.com>
2025-07-09 15:23:08 +0700

    refactor(page): update url env

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,9 @@
 import { Metadata } from 'next'
-import { ENV } from '@/lib/constants'
+import { PAGE_URLS } from '@/types/environment'
 
 const title = '404'
-const description = 'Sorry, i couldn’t find the page you’re looking for.'
-const url = `${ENV.NEXT_PUBLIC_WEBSITE_URL}/404`
+const description = "Sorry, I couldn't find the page you're looking for."
+const url = PAGE_URLS.NOT_FOUND
 
 export const metadata: Metadata = {
   title,
@@ -24,7 +24,7 @@ export default function NotFound() {
     <section className='h-dvh flex items-center justify-center flex-col'>
       <div className='relative text-center'>
         <h1 className='mt-4 text-3xl font-bold tracking-tight text-foreground sm:text-5xl'>Page not found :(</h1>
-        <p className='mt-6 text-base leading-7 text-off-white'>Sorry, i couldn’t find the page you’re looking for.</p>
+        <p className='mt-6 text-base leading-7 text-off-white'>Sorry, I couldn&apos;t find the page you&apos;re looking for.</p>
         <div className='absolute w-full h-full bg-secondary blur-2xl top-0 left-0 -z-10 rounded-full' />
       </div>
     </section>
