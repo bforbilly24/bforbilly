8aa011b219dec88c3b52552566f9c93e25c2340c
Halim <halimputra0701@gmail.com>
2025-07-09 15:49:11 +0700

    feat(component): adding component organisms sign in section

---
+++ src/components/organisms/sign-in-section.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { SignIn } from '@clerk/nextjs'
+
+export function SignInSection() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-background">
+      <div className="w-full max-w-md">
+        <SignIn />
+      </div>
+    </div>
+  )
+}
