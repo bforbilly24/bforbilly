75551ac5f5f02bbe939ac237bebfa4d965ba9bbc
Halim <halimputra0701@gmail.com>
2025-07-09 15:41:29 +0700

    feat(component): adding component atoms theme wrapper

--- src/components/atoms/theme-wrapper.tsx
+++ src/components/atoms/theme-wrapper.tsx
@@ -1,9 +1,18 @@
 'use client'
 
 import * as React from 'react'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { type ThemeProviderProps } from 'next-themes/dist/types'
+import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from 'next-themes'
 
 export function ThemeWrapper({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider 
+      {...props}
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </NextThemesProvider>
+  )
 }
