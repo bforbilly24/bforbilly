c9d31701f8a0b23782bda1ac2720069013973937
Halim <halimputra0701@gmail.com>
2025-07-09 15:30:59 +0700

    refactor(page): update content, update fetch wakatime, adding loading, update export alias

--- src/app/coding-activity/partials/index.ts
+++ src/app/coding-activity/partials/index.ts
@@ -1,4 +1,7 @@
+// export * from './language-analytics'
+// export * from './daily-timeline'
 export * from './languages'
 export * from './activity'
 export * from './code-editor'
 export * from './operating-systems'
+export { default as TotalStats } from './total-stats';
