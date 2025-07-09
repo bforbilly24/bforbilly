4548aa0c1cdc578d219fdc8f8bbf120877a18aa8
Halim <halimputra0701@gmail.com>
2025-07-09 15:26:11 +0700

    refactor(layout): update icon, adding page loading, update url environment

--- src/app/articles/[slug]/layout.tsx
+++ src/app/articles/[slug]/layout.tsx
@@ -1,51 +1,52 @@
-import { allArticles } from 'contentlayer/generated'
-import { Suspense } from 'react'
+import { allArticles } from 'contentlayer/generated';
+import { Suspense } from 'react';
 
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/atoms/accordion'
-import { AsideLink } from '@/components/atoms/aside-link'
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/atoms/accordion';
+import { AsideLink } from '@/components/atoms/aside-link';
 
-import { FadeInStagger, FadeIn } from '@/components/atoms/fade-in'
-import { slugify } from '@/lib/slug'
+import { FadeInStagger, FadeIn } from '@/components/atoms/fade-in';
+import { slugify } from '@/utils/slug';
+import { PageLoading } from '@/components/atoms/page-loading';
 
 async function getContent(params: { slug: string }) {
-  const article = allArticles.find(article => article.slug.toLowerCase() === params.slug)
-  if (!article) null
-  return article
+	const article = allArticles.find(article => article.slug.toLowerCase() === params.slug);
+	if (!article) null;
+	return article;
 }
 
 export default async function ArticleDetailLayout({ children, params }: { children: React.ReactNode; params: { slug: string } }) {
-  const content = await getContent(params)
-  if (!content) return null
+	const content = await getContent(params);
+	if (!content) return null;
 
-  return (
-    <section className='grid grid-cols-12 overflow-hidden h-full'>
-      <aside className='md:col-span-3 lg:col-span-2 border-r border-lines md:block hidden overflow-y-auto'>
-        <Accordion type='single' collapsible defaultValue='table-of-contents'>
-          <AccordionItem value='table-of-contents'>
-            <AccordionTrigger className='border-b border-lines px-5 py-2.5 text-left'>Table of content</AccordionTrigger>
-            <AccordionContent className='mt-5 space-y-1'>
-              <FadeInStagger faster>
-                {content.headings.map((heading: { text: string; level: string }, i: number) => (
-                  <FadeIn key={i}>
-                    <Suspense fallback={<>Loading...</>}>
-                      <AsideLink
-                        href={'#' + slugify(heading.text)}
-                        startWith={`/projects/${params.slug}`}
-                        title={heading.text}
-                        data-level={heading.level}
-                        className='data-[level=three]:pl-8 data-[level=four]:pl-10 text-xs'
-                      >
-                        {heading.text}
-                      </AsideLink>
-                    </Suspense>
-                  </FadeIn>
-                ))}
-              </FadeInStagger>
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-      </aside>
-      <section className='md:col-span-9 lg:col-span-10 col-span-12 overflow-y-auto relative h-[80dvh] md:h-auto scroll-smooth'>{children}</section>
-    </section>
-  )
+	return (
+		<section className='grid h-full grid-cols-12 overflow-hidden'>
+			<aside className='border-lines hidden overflow-y-auto border-r md:col-span-3 md:block lg:col-span-2'>
+				<Accordion type='single' collapsible defaultValue='table-of-contents'>
+					<AccordionItem value='table-of-contents'>
+						<AccordionTrigger className='border-lines border-b px-5 py-2.5 text-left'>Table of content</AccordionTrigger>
+						<AccordionContent className='mt-5 space-y-1'>
+							<FadeInStagger faster>
+								{content.headings.map((heading: { text: string; level: string }, i: number) => (
+									<FadeIn key={i}>
+										<Suspense
+											fallback={
+												<div className='flex h-full flex-1 items-center justify-center'>
+													<PageLoading />
+												</div>
+											}
+										>
+											<AsideLink href={'#' + slugify(heading.text)} startWith={`/projects/${params.slug}`} title={heading.text} data-level={heading.level} className='text-xs data-[level=four]:pl-10 data-[level=three]:pl-8'>
+												{heading.text}
+											</AsideLink>
+										</Suspense>
+									</FadeIn>
+								))}
+							</FadeInStagger>
+						</AccordionContent>
+					</AccordionItem>
+				</Accordion>
+			</aside>
+			<section className='relative col-span-12 h-[80dvh] overflow-y-auto scroll-smooth md:col-span-9 md:h-auto lg:col-span-10'>{children}</section>
+		</section>
+	);
 }
--- src/app/articles/layout.tsx
+++ src/app/articles/layout.tsx
@@ -1,75 +1,83 @@
-'use client'
-import { RiReactjsLine, RiArticleLine } from 'react-icons/ri'
-import { SiTailwindcss } from "react-icons/si";
-import { LiaBookSolid } from 'react-icons/lia'
-import { Suspense } from 'react'
-import { useParams } from 'next/navigation'
+'use client';
+import { RiReactjsLine, RiArticleLine } from 'react-icons/ri';
+import { SiTailwindcss } from 'react-icons/si';
+import { LiaBookSolid } from 'react-icons/lia';
+import { Suspense } from 'react';
+import { useParams } from 'next/navigation';
 
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/atoms/accordion'
-import { AsideLink } from '@/components/atoms/aside-link'
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/atoms/accordion';
+import { AsideLink } from '@/components/atoms/aside-link';
 
-import { FadeInStagger, FadeIn } from '@/components/atoms/fade-in'
+import { FadeInStagger, FadeIn } from '@/components/atoms/fade-in';
+import { PageLoading } from '@/components/atoms/page-loading';
+import { TbBrandNextjs } from 'react-icons/tb';
 
 export default function ArticleLayout({ children }: { children: React.ReactNode }) {
-  const params = useParams()
-  if (params.slug) return children
+	const params = useParams();
+	if (params.slug) return children;
 
-  return (
-    <section className='grid grid-cols-12 overflow-hidden h-full'>
-      <aside className='md:col-span-3 lg:col-span-2 border-r border-lines md:block hidden overflow-y-auto'>
-        <Accordion type='single' collapsible defaultValue='item-0'>
-          {TAGS.map((item, i) => (
-            <AccordionItem value={`item-${i}`} key={i}>
-              <AccordionTrigger className='border-b border-lines px-5 py-2.5 text-left' data-umami-event='accordion-articles'>
-                {item.title}
-              </AccordionTrigger>
-              <AccordionContent className='mt-5 space-y-1'>
-                <FadeInStagger faster>
-                  {item.list.map((listItem, j) => (
-                    <FadeIn key={j}>
-                      <Suspense fallback={<>Loading...</>}>
-                        <AsideLink href={listItem.href} startWith='/projects' title={listItem.title}>
-                          <listItem.icon className='w-4 h-4' />
-                          {listItem.title}
-                        </AsideLink>
-                      </Suspense>
-                    </FadeIn>
-                  ))}
-                </FadeInStagger>
-              </AccordionContent>
-            </AccordionItem>
-          ))}
-        </Accordion>
-      </aside>
-      <section className='md:col-span-9 lg:col-span-10 col-span-12 overflow-y-auto relative h-[84dvh] md:h-auto'>{children}</section>
-    </section>
-  )
+	return (
+		<section className='grid h-full grid-cols-12 overflow-hidden'>
+			<aside className='border-lines hidden overflow-y-auto border-r md:col-span-3 md:block lg:col-span-2'>
+				<Accordion type='single' collapsible defaultValue='item-0'>
+					{TAGS.map((item, i) => (
+						<AccordionItem value={`item-${i}`} key={i}>
+							<AccordionTrigger className='border-lines border-b px-5 py-2.5 text-left' data-umami-event='accordion-articles'>
+								{item.title}
+							</AccordionTrigger>
+							<AccordionContent className='mt-5 space-y-1'>
+								<FadeInStagger faster>
+									{item.list.map((listItem, j) => (
+										<FadeIn key={j}>
+											<Suspense
+												fallback={
+													<div className='flex h-full flex-1 items-center justify-center'>
+														<PageLoading />
+													</div>
+												}
+											>
+												<AsideLink href={listItem.href} startWith='/projects' title={listItem.title}>
+													<listItem.icon className='h-4 w-4' />
+													{listItem.title}
+												</AsideLink>
+											</Suspense>
+										</FadeIn>
+									))}
+								</FadeInStagger>
+							</AccordionContent>
+						</AccordionItem>
+					))}
+				</Accordion>
+			</aside>
+			<section className='relative col-span-12 h-[84dvh] overflow-y-auto md:col-span-9 md:h-auto lg:col-span-10'>{children}</section>
+		</section>
+	);
 }
 
 const TAGS = [
-  {
-    title: 'Article Tags',
-    list: [
-      {
-        title: 'All article',
-        href: '/articles',
-        icon: RiArticleLine
-      },
-      {
-        title: 'React',
-        href: '/articles?tag=Next',
-        icon: RiReactjsLine
-      },
-      {
-        title: 'Tailwind',
-        href: '/articles?tag=Tailwind',
-        icon: SiTailwindcss
-      },
-      {
-        title: 'Non Technical',
-        href: '/articles?tag=Non Technical',
-        icon: LiaBookSolid
-      }
-    ]
-  }
-]
+	{
+		title: 'Article Tags',
+		list: [
+			{
+				title: 'All article',
+				href: '/articles',
+				icon: RiArticleLine,
+			},
+			{
+				title: 'Next.js',
+				href: '/articles?tag=Next.js',
+				icon: TbBrandNextjs,
+			},
+			{
+				title: 'TailwindCSS',
+				href: '/articles?tag=TailwindCSS',
+				icon: SiTailwindcss,
+			},
+			{
+				title: 'Non Technical',
+				href: '/articles?tag=Non Technical',
+				icon: LiaBookSolid,
+			},
+		],
+	},
+];
