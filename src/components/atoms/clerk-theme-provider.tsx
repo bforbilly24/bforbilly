56d247cde8306774127462693e2570cbe81066ee
Halim <halimputra0701@gmail.com>
2025-07-09 15:24:27 +0700

    feat(context): adding clerk theme provider

---
+++ src/components/atoms/clerk-theme-provider.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { ClerkProvider } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
+import { useTheme } from 'next-themes';
+import { ReactNode } from 'react';
+
+interface ClerkThemeProviderProps {
+	children: ReactNode;
+}
+
+export function ClerkThemeProvider({ children }: ClerkThemeProviderProps) {
+	const { resolvedTheme } = useTheme();
+	
+	return (
+		<ClerkProvider
+			appearance={{
+				baseTheme: resolvedTheme === 'dark' ? [dark] : undefined,
+			}}
+		>
+			{children}
+		</ClerkProvider>
+	);
+}
