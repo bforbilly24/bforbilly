d2f44d491bd22f14904f826f4c82b5255fa623b1
Halim <halimputra0701@gmail.com>
2025-07-13 01:40:00 +0700

    fix(page): fix layout cant show navbar and footer

--- src/components/organisms/home/homepage-section.tsx
+++ src/components/organisms/home/homepage-section.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { FadeIn } from '@/components/atoms/fade-in';
+import { FadeIn, FadeInStagger } from '@/components/atoms/fade-in';
 import { GridPattern } from '@/components/atoms/grid-pattern';
 import { ClientOnly } from '@/components/atoms/client-only';
 import { ChevronRightIcon } from 'lucide-react';
@@ -37,38 +37,40 @@ export function HomepageSection() {
 		>
 			{loading && <SvgMotion />}
 			{!loading && (
-				<section id='hero' className='flex flex-col items-center justify-center gap-20 p-5 md:flex-col lg:flex-row'>
-					<FadeIn>
-						<div className='relative z-10 space-y-8 md:space-y-10'>
-							<header className='flex flex-col items-start justify-center'>
-								<div className='flex items-start justify-center'>
-									<AnimatedShinyText>P Hu!. I am</AnimatedShinyText>
+				<section id='hero' className='grid h-full grid-cols-12 overflow-hidden'>
+					<FadeInStagger className='relative col-span-12 flex h-[84dvh] flex-col items-center justify-center gap-20 overflow-y-auto p-5 md:flex-col lg:flex-row' faster>
+						<FadeIn>
+							<div className='space-y-8 md:space-y-10'>
+								<header className='flex flex-col items-start justify-center'>
+									<div className='flex items-start justify-center'>
+										<AnimatedShinyText>P Hu!. I am</AnimatedShinyText>
+									</div>
+
+									<HyperText className='text-3xl font-medium sm:text-4xl md:text-6xl'>Halim Putra</HyperText>
+
+									<div className='mt-4 flex items-center justify-center gap-x-3'>
+										<ChevronRightIcon className='animate-pulse' />
+										<TrueFocus sentence='Frontend developer' manualMode={!isMobile} blurAmount={5} animationDuration={0.4} pauseBetweenAnimations={1.2} />
+									</div>
+
+									<div className='absolute left-0 top-0 -z-10 h-1/2 w-full animate-pulse rounded-full bg-muted-foreground/10 blur-2xl' />
+								</header>
+
+								<div className='space-y-2 text-xs sm:text-sm'>
+									<p className='text-muted-foreground'>{`// wanna see it? visit on my Github page`}</p>
+									<p className='text-muted-foreground'>
+										<span className='text-orange-500'>const</span> <span className='text-purple-500'>githubLink</span> = &apos;
+										<Link target='_blank' href='https://github.com/bforbilly24/bforbilly' className='text-light-brown transition-colors hover:text-foreground hover:underline'>
+											https://github.com/bforbilly24/bforbilly
+										</Link>
+										&apos;
+									</p>
 								</div>
-
-								<HyperText className='text-3xl font-medium sm:text-4xl md:text-6xl'>Halim Putra</HyperText>
-
-								<div className='mt-4 flex items-center justify-center gap-x-3'>
-									<ChevronRightIcon className='animate-pulse' />
-<TrueFocus sentence='Frontend developer' manualMode={!isMobile} blurAmount={5} animationDuration={0.4} pauseBetweenAnimations={1.2} />
-								</div>
-
-								<div className='absolute left-0 top-0 -z-10 h-1/2 w-full animate-pulse rounded-full bg-muted-foreground/10 blur-2xl' />
-							</header>
-
-							<div className='space-y-2 text-xs sm:text-sm'>
-								<p className='text-muted-foreground'>{`// wanna see it? visit on my Github page`}</p>
-								<p className='text-muted-foreground'>
-									<span className='text-purple'>const</span> <span className='text-green'>githubLink</span> = &apos;
-									<Link target='_blank' href='https://github.com/bforbilly24/bforbilly' className='text-light-brown transition-colors hover:text-foreground hover:underline'>
-										https://github.com/bforbilly24/bforbilly
-									</Link>
-									&apos;
-								</p>
 							</div>
-						</div>
-					</FadeIn>
-					<Badge3D />
-					<GridPattern className='absolute inset-x-0 -top-14 -z-10 h-full w-full fill-neutral-100 stroke-neutral-700/5 [mask-image:linear-gradient(to_bottom_right,white_40%,transparent_50%)] dark:fill-secondary/20 dark:stroke-secondary/30' yOffset={-96} interactive />
+						</FadeIn>
+						<Badge3D />
+						<GridPattern className='absolute inset-x-0 -top-14 -z-10 h-full w-full fill-neutral-100 stroke-neutral-700/5 [mask-image:linear-gradient(to_bottom_right,white_40%,transparent_50%)] dark:fill-secondary/20 dark:stroke-secondary/30' yOffset={-96} interactive />
+					</FadeInStagger>
 				</section>
 			)}
 		</ClientOnly>
