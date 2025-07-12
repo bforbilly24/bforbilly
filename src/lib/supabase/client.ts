927f5484bace16b5bad3d24177423113c7c5adac
Halim <halimputra0701@gmail.com>
2025-07-12 23:06:37 +0700

    refactor(lib): adding lib supabase client

---
+++ src/lib/supabase/client.ts
@@ -0,0 +1,8 @@
+import { createBrowserClient } from "@supabase/ssr";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const createClient = () => {
+  return createBrowserClient(supabaseUrl!, supabaseKey!);
+};
