d5570531dd8c2de9dc56b0ffefbf2bdcfd754cbc
Halim <halimputra0701@gmail.com>
2025-07-13 18:11:11 +0700

    refactor(middleware): update minimal middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-// Define routes that require authentication
-const isProtectedRoute = createRouteMatcher([
-	'/guest-book(.*)',
-]);
-
-export default clerkMiddleware(async (auth, req) => {
-	// Protect authenticated routes
-	if (isProtectedRoute(req)) {
-		await auth.protect();
-	}
-});
+export default clerkMiddleware();
 
 export const config = {
-	matcher: [
-		// Skip Next.js internals and all static files, unless found in search params
-		'/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-		// Always run for API routes
-		'/(api|trpc)(.*)',
-	],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
