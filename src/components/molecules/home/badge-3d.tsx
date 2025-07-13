66f6f6c02941fb9616adfbcf96b491b4648806f8
Halim <halimputra0701@gmail.com>
2025-07-13 17:22:15 +0700

    refactor(component): update molecules badge 3d, adding memo

--- src/components/molecules/home/badge-3d.tsx
+++ src/components/molecules/home/badge-3d.tsx
@@ -1,12 +1,12 @@
 'use client';
-import { Suspense } from 'react';
+import { Suspense, memo } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { Environment, Lightformer } from '@react-three/drei';
+import { Environment, Lightformer, Preload } from '@react-three/drei';
 import { Physics } from '@react-three/rapier';
 import { Loader2 } from 'lucide-react';
 import { Band } from '@/components/atoms/band';
 
-const Badge3D = () => {
+const Badge3D = memo(() => {
 	return (
 		<div
 			className='relative h-[32rem] w-full max-w-lg lg:h-full lg:w-full 2xl:h-[48rem]'
@@ -33,6 +33,15 @@ const Badge3D = () => {
 						backgroundColor: 'transparent',
 						touchAction: 'none',
 					}}
+					gl={{ 
+						antialias: false, 
+						alpha: true,
+						powerPreference: 'high-performance',
+						stencil: false,
+						depth: true
+					}}
+					dpr={[1, 2]}
+					performance={{ min: 0.5 }}
 				>
 					<ambientLight intensity={Math.PI} />
 					<Physics interpolate gravity={[0, -40, 0]} timeStep={1 / 60}>
@@ -44,10 +53,13 @@ const Badge3D = () => {
 						<Lightformer intensity={3} color='white' position={[1, 1, 1]} rotation={[0, 0, Math.PI / 3]} scale={[100, 0.1, 1]} />
 						<Lightformer intensity={10} color='white' position={[-10, 0, 14]} rotation={[0, Math.PI / 2, Math.PI / 3]} scale={[100, 10, 1]} />
 					</Environment>
+					<Preload all />
 				</Canvas>
 			</Suspense>
 		</div>
 	);
-};
+});
+
+Badge3D.displayName = 'Badge3D';
 
 export { Badge3D };
