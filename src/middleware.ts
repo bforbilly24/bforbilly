961aa57d3d7ac956ee6bc30a746af02d62419084
Halim <halimputra0701@gmail.com>
2025-07-14 16:54:13 +0700

    refactor(middleware): remove protected route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,6 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher([
-  '/guest-book(.*)',
-  '/api/guest-book(.*)',
-]);
-
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) {
-    await auth.protect();
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
