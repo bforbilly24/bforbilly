fc435f161a93342b25e2fb68b5efea1b0140bead
Halim <halimputra0701@gmail.com>
2025-07-14 18:37:57 +0700

    refactor(middleware): update clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,7 @@ import { clerkMiddleware } from '@clerk/nextjs/server';
 
 export default clerkMiddleware();
 
+// Temporarily disable middleware matching
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: [],
 };
